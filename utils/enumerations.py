bb9353b0e5b16155b7c2adc4ccf4916bcbf7ed9d
DmitriyGrigoriev <dimag@inbox.ru>
2025-01-20 21:24:14 +0300

    chore: Add VehicleDDDView for create and edit vehicle data by DDD.

--- utils/enumerations.py
+++ utils/enumerations.py
@@ -24,9 +24,25 @@
 #
 ##############################################################################
 from enum import Enum
+from typing import Dict
 
 
 class ChoiceEnum(Enum):
     @classmethod
     def choices(cls):
-        return tuple((x.value, x.value) for x in cls)
+        return tuple((x.name, x.value) for x in cls)
+
+    @classmethod
+    def get_value(cls, key):
+        return getattr(cls, key, key).value if hasattr(cls, key) else key
+
+    @classmethod
+    def get_names(cls):
+        return [x.name for x in cls]
+
+    @classmethod
+    def get_values(cls):
+        return [x.value for x in cls]
+
+    def __deepcopy__(self, memodict: Dict = None) -> 'ChoiceEnum':
+        return self
